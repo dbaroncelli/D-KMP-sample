afabf9b8cf6c9de1233e31819e2340ea681a3752
Daniele Baroncelli <daniele.baroncelli@gmail.com>
2021-03-28 12:32:56 +0200

    added comments

--- shared/src/commonMain/kotlin/eu/baroncelli/dkmpsample/shared/viewmodel/screens/countrieslist/CountriesListStateProvider.kt
+++ shared/src/commonMain/kotlin/eu/baroncelli/dkmpsample/shared/viewmodel/screens/countrieslist/CountriesListStateProvider.kt
@@ -5,6 +5,7 @@ import eu.baroncelli.dkmpsample.shared.viewmodel.StateProvider
 
 fun StateProvider.getCountriesListState() : CountriesListState {
 
+    // the screen gets initialized only the first time it gets called
     return stateManager.getScreen(
         initState = { CountriesListState(isLoading = true) },
         callOnInit = { events.loadCountriesListData() }
--- shared/src/commonMain/kotlin/eu/baroncelli/dkmpsample/shared/viewmodel/screens/countrydetail/CountryDetailStateProvider.kt
+++ shared/src/commonMain/kotlin/eu/baroncelli/dkmpsample/shared/viewmodel/screens/countrydetail/CountryDetailStateProvider.kt
@@ -5,6 +5,8 @@ import eu.baroncelli.dkmpsample.shared.viewmodel.StateProvider
 
 fun StateProvider.getCountryDetailState(country : String) : CountryDetailState {
 
+    // the screen gets initialized the first time it gets called
+    // and if the "reinitWhen" condition is true
     return stateManager.getScreen(
         initState = { CountryDetailState(countryName = country, isLoading = true) },
         callOnInit = { events.loadCountryDetailData(country) },
