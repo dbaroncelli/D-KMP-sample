fd5b2434c719fae46936b6dbfe0b7ebd28b6842e
Daniele Baroncelli <daniele.baroncelli@gmail.com>
2021-05-07 14:01:04 +0300

    small change

--- shared/src/commonMain/kotlin/eu/baroncelli/dkmpsample/shared/viewmodel/ScreenEnum.kt
+++ shared/src/commonMain/kotlin/eu/baroncelli/dkmpsample/shared/viewmodel/ScreenEnum.kt
@@ -2,11 +2,7 @@ package eu.baroncelli.dkmpsample.shared.viewmodel
 
 internal val startScreen = Screen.CountriesList // the start screen should be specified here
 
-// if you want to support a "two pane" visualization on larger devices:
-// for each detail screen (to be shown on the right) define a master screen (to be shown on the left)
-// N.B. "two pane" visualization is currently not provided by the architecture, but it will in the future
-
-enum class Screen(val route: String, val requiresInstanceId : Boolean = false, val masterScreen: Screen? = null) {
+enum class Screen(val route: String, val requiresInstanceId : Boolean = false) {
     CountriesList("countrieslist"),
-    CountryDetail("country", true, CountriesList),
+    CountryDetail("country", true),
 }
