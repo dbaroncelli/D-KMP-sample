bedc1c6f73b6c6296dcfa951bab05f5c72b1f518
Daniele Baroncelli <daniele.baroncelli@gmail.com>
2021-05-21 12:28:26 +0300

    UI fix on iOS

--- iosApp/iosApp/views/screens/countrieslist/CountriesListScreen.swift
+++ iosApp/iosApp/views/screens/countrieslist/CountriesListScreen.swift
@@ -14,38 +14,39 @@ struct CountriesListScreen: View {
     var onFavoriteIconClick : (String) -> Void
     
     var body: some View {
-        if countriesListState.isLoading {
-            LoadingScreen()
-        } else {
-            List {
-                if countriesListState.countriesListItems.count == 0 {
-                    HStack(spacing: 0) {
-                        Text("empty list")
-                    }
-                } else {
-                    Section(header: CountriesListHeader()) {
-                        ForEach (countriesListState.countriesListItems, id: \.name) { item in
-                            NavigationLink( linkFunction: { onListItemClick(item.name) } ) {
-                                CountriesListRow(
-                                    item: item,
-                                    favorite: countriesListState.favoriteCountries[item.name] != nil,
-                                    onFavoriteIconClick: { onFavoriteIconClick(item.name) }
-                                )
+        VStack {
+            if countriesListState.isLoading {
+                LoadingScreen()
+            } else {
+                List {
+                    if countriesListState.countriesListItems.count == 0 {
+                        HStack(spacing: 0) {
+                            Text("empty list")
+                        }
+                    } else {
+                        Section(header: CountriesListHeader()) {
+                            ForEach (countriesListState.countriesListItems, id: \.name) { item in
+                                NavigationLink( linkFunction: { onListItemClick(item.name) } ) {
+                                    CountriesListRow(
+                                        item: item,
+                                        favorite: countriesListState.favoriteCountries[item.name] != nil,
+                                        onFavoriteIconClick: { onFavoriteIconClick(item.name) }
+                                    )
+                                }
                             }
                         }
                     }
                 }
             }
-            .navigationBarTitle("D-KMP sample", displayMode: .inline)
-            .toolbar {
-                ToolbarItemGroup(placement: .bottomBar) {
-                    CountriesListBottomBar(
-                        selectedTab : getScreenIdentifier(.countrieslist, countriesListState.params),
-                        onItemClick: { menuItem in onMenuItemClick(menuItem) }
-                    )
-                }
+        }
+        .navigationBarTitle("D-KMP sample", displayMode: .inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                CountriesListBottomBar(
+                    selectedTab : getScreenIdentifier(.countrieslist, countriesListState.params),
+                    onItemClick: { menuItem in onMenuItemClick(menuItem) }
+                )
             }
-
         }
     }
     
