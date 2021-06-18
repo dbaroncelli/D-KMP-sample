d1047c1e058772b31d8d7b19cba3a9358ebf67ad
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-06-18 10:31:26 +0430

    Update to Compose 1.0.0-beta08

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val min_sdk = 21
     const val target_sdk = 30
 
-    const val compose = "1.0.0-beta07"
+    const val compose = "1.0.0-beta08"
     const val kotlin = "1.4.32"
     const val ktor = "1.5.3"
     const val sql_delight = "1.5.0"
