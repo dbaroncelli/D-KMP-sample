abb23285e104717a0bc3c786dd9a174d0e8b178e
Luca Spinazzola <mspinluca@gmail.com>
2023-09-11 23:49:50 -0400

    use dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,14 @@ dependencyResolutionManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 rootProject.name = "D-KMP-sample"
 
 include(":androidApp")
