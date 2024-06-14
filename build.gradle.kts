aa3f62bb6c0b15e93cfbe1d6d567fd551ba43199
Daniele Baroncelli <daniele.baroncelli@gmail.com>
2024-06-14 18:49:33 +0200

    Upgraded to Kotlin 2.0 and Compose 1.6.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
-group = "eu.baroncelli.dkmpsample"
-version = "1.0-SNAPSHOT"
-
 plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.sqlDelight) apply false
     alias(libs.plugins.skie) apply false
 }
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 dependencies {
